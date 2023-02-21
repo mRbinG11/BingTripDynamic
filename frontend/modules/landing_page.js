1e6a573e1a01ef8de35d85bf934f1ddf63e614bc
CrioUser <crio-user@criodo.com>
2023-02-22 00:11:09 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,8 +1,13 @@
 import config from "../conf/index.js";
 
 async function init() {
+  console.log("From init()");
+  console.log(config.backendEndpoint + "/cities");
+
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
+
+  console.log(cities);
 
   //Updates the DOM with the cities
   cities.forEach((key) => {
@@ -14,14 +19,33 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    let response = await fetch(config.backendEndpoint + "/cities");
+    let json = await response.json();
+    return json;
+  } catch (err) {
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
+  let dataDiv = document.getElementById('data');
+  let cardDiv = document.createElement('div');
+  cardDiv.setAttribute('class', 'col-sm-6 col-lg-3 mb-4');
+  cardDiv.innerHTML = `<a href="pages/adventures/?city=${id}" id=${id}>
+    <div class="tile">
+      <div class="tile-text text-center">
+        <h5>${city}</h5>
+        <p>${description}</p>
+      </div>
+      <img src=${image} alt=${city} class="img-responsive">
+    </div>
+  </a>
+`;
+  dataDiv.appendChild(cardDiv);
 }
 
 export { init, fetchCities, addCityToDOM };
